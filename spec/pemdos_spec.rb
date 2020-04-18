f80c1f0654a31aa487ed6db4b1d55b971ec0c0ac
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-18 14:02:03 +0000

    Done.

--- spec/pemdos_spec.rb
+++ spec/pemdos_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../fix_using_tests/pemdos'
+require 'pry'
 
 describe "pemdos" do
-  it "`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
+  it"`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
   expect(snake_it_up("surprise!")).to eq("sssssssssssurprise!")
   end
 end
