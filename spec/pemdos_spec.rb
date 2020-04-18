83df773d30a604e26298e769f0e48bbb1ff02a57
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-18 13:30:40 +0000

    Automatically backed up by Learn

--- spec/pemdos_spec.rb
+++ spec/pemdos_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../fix_using_tests/pemdos'
+require 'pry'
 
 describe "pemdos" do
   it "`snake_it_up` adds the letter 's' ten times before any word that starts with 's'" do
   expect(snake_it_up("surprise!")).to eq("sssssssssssurprise!")
   end
+  binding.pry
 end
